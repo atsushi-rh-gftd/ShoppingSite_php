23e2da7900e4dae182223c2ca5d3b712a56a600c
Atsushi Hasumoto <a.hasumoto@gftd.works>
2022-08-16 14:33:10 +0900

    Second commit. item_list.php etc..

---
+++ common.php
@@ -0,0 +1,10 @@
+<?php
+	$dsn = "mysql:host=192.168.3.122;dbname=testdb;charset=utf8";
+	$db_user = "testuser";
+	$db_passwd = "testuser1234";
+
+
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+/* login check ...*/
+if(empty($_POST['login_id'] && $_POST['login_passwd']))
+   header("Location: index.php");
+   else {
+       /* POST variables into variables */
+       $login = $_POST['login_id'];
+       $passwd = $_POST['login_passwd'];
+       var_dump($_POST);
+	}
+include_once 'common.php';
+$result;
+try {
+    /* DB query get name, password ....*/
+    
+    
+   
+    $pdo = new PDO($dsn,$db_user,$db_passwd);
+    $stmt =$pdo->prepare("SELECT password FROM test_mysql01 where name = ? and password =?;");
+    $stmt->bindParam(1, $login, PDO::PARAM_STR);
+    $stmt->bindParam(2, $passwd, PDO::PARAM_STR);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    var_dump($result);
+    
+}catch (PDOException $error)
+{
+    echo $error->getMessage();
+}
+?>
+
+<?php
+    if($result[0]['password'] == $passwd)
+    {
+        session_start();
+        $_SESSION['login'] = true;
+        header("Location: item_list.php");
+    }
+    else
+        header("Location: index.php");
+    
+?>
+    
